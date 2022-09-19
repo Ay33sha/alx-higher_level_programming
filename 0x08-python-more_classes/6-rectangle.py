c43972162c2df635df1a7173eb5d401bbce26c08
Aisha Nasir <nasircutieeesha@gmail.com>
2022-09-19 16:29:15 -0700

    a class Rectangle that defines a rectangle by: (based on 5-rectangle.py)

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,6 +4,9 @@
 
 class Rectangle:
     """ Represent a Rectangle object """
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initialize a new Rectangle object
 
